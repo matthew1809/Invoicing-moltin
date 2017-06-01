b591e6cfbc52dcc3ba9485dccf851e55387da5b3
Matthew Foyle <matt@moltin.com>
2017-06-01 16:32:20 +0100

    first pass

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var https   = require("https");
 var fs      = require("fs");
+var bodyParser = require('body-parser');
+var config = require('./config.js')
+const moltin = require('@moltin/sdk');
+const Moltin = moltin.gateway({
+  client_id: config.client_id,
+  client_secret: config.client_secret,
+});
 
 var app = express()
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 function generateInvoice(invoice, filename, success, error) {
     var postData = JSON.stringify(invoice);
@@ -41,32 +50,62 @@ function generateInvoice(invoice, filename, success, error) {
 
 var invoice = {
     logo: "http://invoiced.com/img/logo-invoice.png",
-    from: "Invoiced\n701 Brazos St\nAustin, TX 78748",
+    from: "My Moltin Store",
     to: "Johnny Appleseed",
     currency: "usd",
     number: "INV-0001",
     payment_terms: "Auto-Billed - Do Not Pay",
-    items: [
-        {
-            name: "Subscription to Starter",
-            quantity: 1,
-            unit_cost: 50
-        }
-    ],
-    fields: {
-        tax: "%"
-    },
-    tax: 5,
+    items: [],
     notes: "Thanks for being an awesome customer!",
     terms: "No need to submit payment. You will be auto-billed for this invoice."
 };
 
-app.get('/', function (req, res) {
-  generateInvoice(invoice, 'invoice.pdf', function() {
-      console.log("Saved invoice to invoice.pdf");
-  }, function(error) {
-      console.error(error);
-  });
+var order_items = {
+    name: "",
+    quantity: 0,
+    unit_cost: 0
+};
+
+var order_id = ''
+
+var get_order_items = function(order_id) {
+  console.log(order_id)
+
+  Moltin.Orders.Items(order_id)
+    .then((items) => {
+      var data = items.data
+      data.forEach((item) => {
+        invoice.items.push({
+              name: item.name,
+              quantity: item.quantity,
+              unit_cost: item.unit_price.amount
+          });
+      })
+    })
+    .then(() => {
+      console.log(invoice.items);
+      generateInvoice(invoice, 'invoice.pdf', function() {
+          console.log("Saved invoice to invoice.pdf");
+      }, function(error) {
+          console.error(error);
+      });
+    })
+
+    .catch((error) => {
+      // 4xx, 5xx response
+      console.log(error);
+    });
+}
+
+app.post('/', function (req, res) {
+  pbody = JSON.parse(req.body.resources);
+  //console.log('The body:', pbody);
+    order_id = pbody.data.id;
+    console.log('the parsed order id is: ' + order_id);
+    invoice.to = pbody.data.customer.name;
+    invoice.currency = pbody.data.meta.display_price.with_tax.currency;
+  return get_order_items(order_id);
+
 });
 
 app.listen(3000, function () {
