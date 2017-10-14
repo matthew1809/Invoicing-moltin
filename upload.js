351283f27bd9299f4e745fa4167b0695ba1c9c86
Matthew Foyle <matt@moltin.com>
2017-10-14 23:27:45 +0100

    add s3 utils for file upload

---
+++ upload.js
@@ -0,0 +1,42 @@
+var fs         = require("fs");
+var AWS        = require('aws-sdk');
+
+AWS.config.update({
+  region: process.env.bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: process.env.IdentityPoolId
+  })
+});
+
+let base64data;
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: process.env.albumBucketName}
+});
+
+var upload = (body) => {
+  s3.upload({
+      Bucket: process.env.albumBucketName,
+      Key: 'invoice.pdf',
+      Body: body,
+      ACL: 'public-read'
+    },function (resp) {
+      console.log(arguments);
+      console.log('Successfully uploaded package.');
+    });
+
+}
+
+var baseFile = (file) => {
+  fs.readFile(file, function (err, data) {
+  if (err) { throw err; }
+
+  base64data = new Buffer(data, 'binary');
+  upload(base64data);
+  })
+
+};
+
+exports.upload = upload;
+exports.baseFile = baseFile;
